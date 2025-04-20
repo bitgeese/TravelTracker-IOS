78853cd389daab1a336e23a1241e25d6abf4a983
Maciej Janowski <maciej.janowski1@icloud.com>
2025-04-20 14:34:39 +0200

    Initial Commit

---
+++ TravelTracker/TravelTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTrackerApp.swift
+//  TravelTracker
+//
+//  Created by Maciej Janowski on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
